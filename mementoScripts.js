ee326100e20c1021c2b48a926cedbf03bfd860f8
Johnny Pham <johnnypham@outlook.com>
2018-05-03 16:08:33 -0700

    'about' button now takes you somewhere, added password validation script, inputs now have requirements

--- mementoScripts.js
+++ mementoScripts.js
@@ -1,3 +1,14 @@
 function construction() {
     alert("Page is under construction!");
 }
+
+
+
+  function check(input) {
+      if (input.value != document.getElementById('passwordForm').value) {
+          input.setCustomValidity("Passwords don't match");
+      } else {
+          // gets rid of alert box if inputs match
+          input.setCustomValidity('');
+      }
+  }
