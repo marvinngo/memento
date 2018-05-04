513b5f1432e77083f0db847b5b10c1f0db168387
evanmorrow <evanmorrow@gmail.com>
2018-05-03 20:19:57 -0700

    Think I actually fixed User_ capitalization consistency issues.

--- register.php
+++ register.php
@@ -11,33 +11,33 @@
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		$user_Name = "";
-        $user_Password = "";
-        $user_Email = "";
-        if (isset($_POST["user_Name"]) && !empty($_POST["user_Name"])) {
+		$User_Name = "";
+        $User_Password = "";
+        $User_Email = "";
+        if (isset($_POST["User_Name"]) && !empty($_POST["User_Name"])) {
             // these names don't all have to be the same but if we have several variables
             // then it makes sense to make them the same
-            $user_Name =  $_POST["user_Name"];
+            $User_Name =  $_POST["User_Name"];
         }
-        if (isset($_POST["user_Password"]) && !empty($_POST["user_Password"])) {
+        if (isset($_POST["User_Password"]) && !empty($_POST["User_Password"])) {
             // these names don't all have to be the same but if we have several variables
             // then it makes sense to make them the same
-            $user_Password =  $_POST["user_Password"];
+            $User_Password =  $_POST["User_Password"];
         }
-        if (isset($_POST["user_Email"]) && !empty($_POST["user_Email"])) {
+        if (isset($_POST["User_Email"]) && !empty($_POST["User_Email"])) {
             // these names don't all have to be the same but if we have several variables
             // then it makes sense to make them the same
-            $user_Email =  $_POST["user_Email"];
+            $User_Email =  $_POST["User_Email"];
         }
 
-        if(empty($user_Name) || empty($user_Password) || empty($user_Email)) {
+        if(empty($User_Name) || empty($User_Password) || empty($User_Email)) {
             echo "<p>No update performed.</p>";
         } else {
             // perform update to the DB
-            $sql = "INSERT INTO `tbl_User` (user_Name, user_Password, user_Email) values (:userName, :userPassword, :userEmail)";
+            $sql = "INSERT INTO `tbl_User` (User_Name, User_Password, User_Email) values (:UserName, :UserPassword, :UserEmail)";
             // $insert is a 'PDOStatement
             $statement = $conn->prepare($sql);
-            $statement->execute(array(":userName" => $user_Name, ":userPassword" => $user_Password, ":userEmail" => $user_Email));
+            $statement->execute(array(":UserName" => $User_Name, ":UserPassword" => $User_Password, ":UserEmail" => $User_Email));
             header( 'Location: createjoin.html' ) ;
         }
 
