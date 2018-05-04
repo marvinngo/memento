5aa7f4748a853b0f376b672112d5e5cef4515fde
Johnny Pham <johnnypham@outlook.com>
2018-05-04 14:35:30 -0700

    modified footer

--- mementoScripts.js
+++ mementoScripts.js
@@ -4,13 +4,12 @@ function construction() {
 
 
 
-  function check(input) {
-      if (input.value != document.getElementById('passwordForm').value) {
-          input.setCustomValidity("Passwords don't match");
-      } else {
-          // gets rid of alert box if inputs match
-          input.setCustomValidity('');
-      }
-  }
+function check(input) {
+    if (input.value != document.getElementById('passwordForm').value){
+        input.setCustomValidity("passwords don't match");
+    }  else {
+        // gets rid of alert box if inputs match
+        input.setCustomValidity('');
+    }
+}
 
-  function 
