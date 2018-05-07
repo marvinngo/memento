a1d6b3e633cf0711b56fda9ed7435576821de596
evanmorrow <evanmorrow@gmail.com>
2018-05-06 17:49:19 -0700

    Added querying to join.html to check if the Group_Name exists and Group_Password is corrects and redirects to mygroups.html or echoes an error message.

--- joingroup.php
+++ joingroup.php
@@ -17,11 +17,11 @@ if ($methodType === 'POST') {
 
 		    $Group_Name = "";
         $Group_Password = "";
-        $Group_Description = "";
-        $Group_Size = "";
+      
         if (isset($_POST["Group_Name"]) && !empty($_POST["Group_Name"])) {
-            // these names don't all have to be the same but if we have several variables
-            // then it makes sense to make them the same
+            // Checks if Group_Name is set (not null) and not empty
+            // If both are true, sets $Group_Name to whatever is in $_POST 
+            // that was sent from a form when a user clicked Submit
             $Group_Name =  $_POST["Group_Name"];
         }
         if (isset($_POST["Group_Password"]) && !empty($_POST["Group_Password"])) {
@@ -29,42 +29,39 @@ if ($methodType === 'POST') {
             // then it makes sense to make them the same
             $Group_Password =  $_POST["Group_Password"];
         }
-        if (isset($_POST["Group_Description"]) && !empty($_POST["Group_Description"])) {
-            // these names don't all have to be the same but if we have several variables
-            // then it makes sense to make them the same
-            $Group_Description =  $_POST["Group_Description"];
-        }
-        if (isset($_POST["Group_Size"]) && !empty($_POST["Group_Size"])) {
-            // these names don't all have to be the same but if we have several variables
-            // then it makes sense to make them the same
-            $Group_Size =  $_POST["Group_Size"];
-        }
 
-        if(empty($Group_Name) || empty($Group_Password) || empty($Group_Description) || empty($Group_Size)) {
+        if(empty($Group_Name) || empty($Group_Password)) {
             echo "<p>No update performed.</p>";
+          
         } else {
           
-            // Check if username already exists in database:
+            // Check if Group Name exists in database:
           
             $sql = "SELECT * FROM tbl_Group WHERE Group_Name = :GroupName";
 
             $statement = $conn->prepare($sql);
             $statement->execute(array(":GroupName" => $Group_Name));
             $count = $statement->rowCount();
+            $rows = $statement->fetchAll();
           
-            if ($count > 0) {
-              echo "Group Name already exists in DB.";
+            if ($count == 0) {
+              echo "Group Name does not exist in DB.";
             } else {
               
-            // perform update to the DB
+            // Check if Password matches in database:
               
-            $sql = "INSERT INTO `tbl_Group` (Group_Name, Group_Description, Group_Password, Group_Size) values (:GroupName, :GroupDescription, :GroupPassword, :GroupSize)";
-            // $insert is a 'PDOStatement
-            $statement = $conn->prepare($sql);
-            $statement->execute(array(":GroupName" => $Group_Name, ":GroupPassword" => $Group_Password, ":GroupDescription" => $Group_Description, ":GroupSize" => $Group_Size));
-            header( 'Location: mygroups.html' ) ;
-            }
-        }
+          if ($count == 1) {
+          if ($Group_Password == $rows[0]['Group_Password']) {
+            // Placeholder - should go to specific group's page? Depends
+            // how MyGroups page is implemented.
+            header( 'Location: http://mementovancouver.com/mygroups.html' ) ;
+          } else {
+            echo "Username matches DB.<br>";
+            echo "incorrect password";
+          }
+          }
+          }
+          }
 
     } catch(PDOException $e) {
         echo "<p style='color: red;'>From the SQL code: $sql</p>";
