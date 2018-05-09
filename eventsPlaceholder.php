0278ba2a37467debe7abf6cdb3d26939006d3418
Mark Kropivnitski <theultimatesizzle@gmail.com>
2018-05-09 16:44:21 -0700

    Added placeholder php file that returns a sample of what look like event JSONs

---
+++ eventsPlaceholder.php
@@ -0,0 +1,15 @@
+<?php
+
+$sampleEvents
+= array(
+    array(
+        'Event_Name' => 'First Event Placeholder',
+        'Event_Description' => 'First Event Description',
+    ),
+    array(
+        'Event_Name' => 'Second Event Placeholder',
+        'Event_Description' => 'Second Event Description',
+    ),
+);
+
+var_dump($sampleEvents);
