6ecc9eafcbb32b21cbb59253100821ec29049f50
unknown <marvinbtngo@gmail.com>
2018-05-09 13:22:15 -0700

    added login modal

--- registration.php
+++ registration.php
@@ -21,33 +21,66 @@
   
 <!-- Navigation -->
 <nav class="navbar navbar-expand-md navbar-dark sticky-top w-100" style="background-color: #1A75C1;">
-    <div class="container-fluid w-75">
-      <a class="navbar-brand" href="index.php"><img src="img/frontpage/logo/logo.png"></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTop">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarTop">
-        <ul class="navbar-nav ml-auto">
-          <div class="dropdown-divider"></div>
-          <li class="nav-item my-0">
-            <a class="nav-link" href="index.php">Home</a>
-          </li>
-          <div class="dropdown-divider"></div>
-          <li class="nav-item my-0">
-            <a class="nav-link" href="createjoin.php">Groups</a>
-          </li>
-          <div class="dropdown-divider"></div>
-          <li class="nav-item my-0">
-            <a class="nav-link" href="registration.php">Sign Up</a>
-          </li>
-          <div class="dropdown-divider"></div>
-          <li class="nav-item my-0">
-            <a class="nav-link" href="#" data-toggle="modal" data-target="#login">Login</a>
-          </li>
-        </ul>
-      </div>
+  <div class="container-fluid w-75">
+    <a class="navbar-brand" href="index.php"><img src="img/frontpage/logo/logo.png"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTop">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTop">
+      <ul class="navbar-nav ml-auto">
+        <div class="dropdown-divider"></div>
+        <li class="nav-item my-0">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <div class="dropdown-divider"></div>
+        <li id="Groups" class="nav-item my-0">
+          <a class="nav-link" href="createjoin.php">Groups</a>
+        </li>
+        <div class="dropdown-divider"></div>
+        <li class="nav-item my-0">
+          <a id="signup" class="nav-link" href="registration.php">Sign Up</a>
+        </li>
+        <div class="dropdown-divider"></div>
+        <li id="ms" class="nav-item my-0">
+          <a class="nav-link" href="#" data-toggle="modal" data-target="#login">Login</a>
+        </li>
+      </ul>
     </div>
+  </div>
 </nav>
+<!-- The Modal -->
+<div class="modal fade" id="login">
+  <div class="modal-dialog">
+    <div class="modal-content">
+    <!-- Modal Header -->
+      <div class="modal-header mx-auto">
+          <div class="container mx-auto">
+              <div class="row">
+                <div class="col-2">
+                </div>
+                <div class="col-8 text-center">
+                   <h3>
+                     Login 
+                   </h3>
+                </div>
+                <div class="col-2">
+                    <button type="button" class="close text-right" data-dismiss="modal">&times;</button>
+                </div>
+              </div>
+              </div>
+      </div>
+      <!-- Modal body -->
+      <div class="modal-body col-6 mx-auto">
+        <form method="post" action="signin.php">
+		  <input type="text" name="User_Name" class="modaluserpw" placeholder="Username" autofocus>
+		  <input type="password" name="User_Password" 
+          class="modaluserpw" placeholder="Password">
+		  <input type="submit" name="login" class="login loginmodal-submit modaluserpw" value="Login">
+		</form>
+    </div>
+    </div>
+  </div>
+</div>
 
 <!-- background div -->
 <div class="registerBG container-fluid">
@@ -158,9 +191,9 @@
     crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="mementoScripts.js"></script>
-    
+
   
   </body>
 </html>
